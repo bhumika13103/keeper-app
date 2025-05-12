b5cdb41d5cea669295bcdf273fee15d939bc8a4b
Bhumika Singal <bhumikasingal1310@gmail.com>
2025-05-12 17:57:40 +0530

    Added multiple notes adding functionality

--- src/Components/App.jsx
+++ src/Components/App.jsx
@@ -1,15 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
 import CreateArea from "./CreateArea";
 
 function App(){
+    const [noteData,setNoteData] = useState([]);
+
+    function handleFormData(data){
+        setNoteData(prev =>{
+            return ([
+                ...prev,
+                data
+            ])
+        });
+        console.log(noteData);
+        
+    }
     return (
         <div>
             <Header />
-            <CreateArea />
-      <Note key={1} title="Note title" content="Note content" />
+            <CreateArea sendData={handleFormData}/>
+            {noteData.map((note)=>{
+                return <Note title={note.title} content={note.content} />
+            })}
+      
       <Footer />
         </div>
     );
--- src/Components/CreateArea.jsx
+++ src/Components/CreateArea.jsx
@@ -1,12 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
-function CreateArea() {
+function CreateArea(props) {
+    const [noteContent, setNoteContent]= useState({
+        title : "",
+        content : ""
+    })
+    function handleChange(event){
+        const {name,value} = event.target;
+        setNoteContent ((prev) => {
+            return ({
+                ...prev,
+                [name]:value,
+            })
+        });
+    }
+    function handleClick(event){
+        props.sendData(noteContent);
+        event.preventDefault();
+    }
   return (
     <div>
       <form>
-        <input name="title" placeholder="Title" />
-        <textarea name="content" placeholder="Take a note..." rows="3" />
-        <button>Add</button>
+        <input onChange={handleChange} name="title" placeholder="Title" value = {noteContent.title} />
+        <textarea onChange={handleChange} name="content" placeholder="Take a note..." rows="3" value={noteContent.content}/>
+        <button onClick={handleClick}>Add</button>
       </form>
     </div>
   );
